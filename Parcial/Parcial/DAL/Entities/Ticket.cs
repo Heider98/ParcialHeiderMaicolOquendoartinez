5b7fbf89bee866793c47ed57d9d50ad7c2b862e7
Heider98 <eider820hotmail.com>
2023-05-21 20:01:24 -0500

    Project creation and ticket class

---
+++ Parcial/Parcial/DAL/Entities/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial.DAL.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Fecha de uso")]
+        public DateTime? UseDate { get; set; }
+
+        [Display(Name = "¿Fué usada?")]
+        public bool IsUsed { get; set; }
+
+        [Display(Name = "Entrada por donde ingreso")]
+        public string? EntranceGate { get; set; }
+    }
+}
