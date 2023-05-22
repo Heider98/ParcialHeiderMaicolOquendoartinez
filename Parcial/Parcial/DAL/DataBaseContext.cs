f81ca2a09f84c95575c2aa4d25412ac31e9c42bb
Heider98 <eider820hotmail.com>
2023-05-21 21:13:32 -0500

    Creation of the Database and its connection.

---
+++ Parcial/Parcial/DAL/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial.DAL.Entities;
+using System.Diagnostics.Metrics;
+
+namespace Parcial.DAL
+{
+    public class DataBaseContext : DbContext 
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(c => c.Id).IsUnique();
+      
+        }
+    }
+}
